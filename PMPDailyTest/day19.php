14b3947c49637ee2c8c7cfbe6b327924e4cf0e52
kanggege1102 <94180289@qq.com>
2019-04-03 08:43:47 +0800

    add

---
+++ PMPDailyTest/day19.php
@@ -0,0 +1,31 @@
+<?php 
+$arr = [1,3,4,6,8,9,13,25,63,79];
+
+$n = 10;
+
+var_dump(sz($arr,$n));
+
+function sz($arr,$n){
+
+	while (strlen($arr)<$str) {
+
+		$res = intval($arr);
+
+		if ($res<$str) {
+			
+			return $res;
+
+		}elseif ($res>$str) {
+			
+			return $arr[];
+
+		}else{
+
+			return $str;
+
+		}
+	}
+}
+
+
+ ?>
