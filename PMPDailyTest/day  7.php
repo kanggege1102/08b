fb2bb4084812d52abe59290b7691a14d0b1cc570
kanggege1102 <94180289@qq.com>
2019-04-18 08:43:53 +0800

    add

---
+++ PMPDailyTest/day  7.php
@@ -0,0 +1,28 @@
+<?php 
+
+$index = 5;
+
+function GetUglyNumber_Solution($index){
+
+$m[] = array(1,2,3,4,5,6,7);
+
+$num = sort($m);
+
+if ($num = 5) {
+	echo 5;
+}else{
+
+}
+return $m;
+
+if ($num/$m==1) {
+	echo "$num是丑数";
+}else{
+	echo "不是丑数";
+}
+
+}
+
+
+
+ ?>
